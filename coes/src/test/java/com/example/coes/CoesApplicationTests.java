b0aa6cbfbf3d9bb33e5ebb22c9a6523718fbf185
sebasjem1234 <sebasjem1234@gmail.com>
2021-12-08 18:09:37 -0500

    recuerdos del pollo

---
+++ coes/src/test/java/com/example/coes/CoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
