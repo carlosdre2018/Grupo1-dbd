bcf73b338c78a745d648de22c46148ebd8ab8bf5
sebasjem1234 <sebasjem1234@gmail.com>
2021-12-08 19:46:27 -0500

    commit

---
+++ coes2/src/test/java/com/example/coes2/Coes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coes2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coes2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
