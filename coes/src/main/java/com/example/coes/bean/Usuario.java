e2e5d92ac2895d3511c7caa58b579610dd7113db
sebasjem1234 <sebasjem1234@gmail.com>
2021-12-08 19:03:21 -0500

    asd

---
+++ coes/src/main/java/com/example/coes/bean/Usuario.java
@@ -0,0 +1,9 @@
+package com.example.coes.bean;
+import java.time.LocalDate;
+public class Usuario {
+    Long ID_USUARIO;
+    String CONTRASENA;
+    LocalDate FECHA_INGRESO;
+    String TIPO_USUARIO;
+    String DIRECCION;
+}
