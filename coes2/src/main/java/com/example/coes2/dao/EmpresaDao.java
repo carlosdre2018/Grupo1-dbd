84487373f891c2a8d6f9828c8123c3050feaaea6
carlos2001 <carlos.chavez.b@uni.pe>
2021-12-08 21:01:40 -0500

    asd

---
+++ coes2/src/main/java/com/example/coes2/dao/EmpresaDao.java
@@ -0,0 +1,67 @@
+package com.example.coes2.dao;
+
+import com.example.coes2.bean.Empresa;
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Jdbc;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmpresaDao implements Dao<Empresa> {
+    @Autowired
+    JdbcTemplate template;
+
+    public List<Empresa> obtenerTodos() {
+        List<Empresa> res = new ArrayList<>();
+        String sql = "SELECT * FROM EMPRESA";
+        SqlRowSet rs = template.queryForRowSet(sql);
+        while (rs.next()) {
+            res.add(obtenerRegistro(rs));
+        }
+        return res;
+    }
+
+    public void agregar(Empresa empresa) {
+        String sql = "INSERT INTO EMPRESA(RAZON_SOCIAL, FECHA_RETIRO, NOMBRE, CORREO, TELEFONO, ID_USUARIO) VALUES(?,?,?,?,?,?)";
+        template.update(sql, empresa.getRAZON_SOCIAL(), empresa.getFECHA_RETIRO(), empresa.getNOMBRE(),
+                empresa.getCORREO(), empresa.getTELEFONO(), empresa.getID_USUARIO());
+    }
+
+    public Empresa buscar(Long id) {
+        Empresa empresa = null;
+        String sql = "SELECT * FROM EMPRESA WHERE ID_EMPRESA=?";
+        SqlRowSet rs = template.queryForRowSet(sql, id);
+        if (rs.next()) {
+            empresa = obtenerRegistro(rs);
+        }
+        return empresa;
+    }
+
+    public void actualizar(Empresa empresa) {
+        String sql = "UPDATE EMPRESA SET RAZON_SOCIAL = ?, TELEFONO=?)" + "WHERE ID_EMPRESA=?";
+        template.update(sql, empresa.getRAZON_SOCIAL(), empresa.getTELEFONO(), empresa.getID_EMPRESA());
+    }
+
+    public void eliminar(Empresa empresa) {
+        String sql = "DELETE FROM EMPRESA WHERE ID_EMPRESA=?";
+        template.update(sql, empresa.getID_EMPRESA());
+    }
+
+    private Empresa obtenerRegistro(SqlRowSet rs) {
+        Empresa empresa = new Empresa();
+        empresa.setID_EMPRESA(rs.getLong("ID_EMPRESA"));
+        empresa.setRAZON_SOCIAL(rs.getString("RAZON_SOCIAL"));
+        empresa.setNOMBRE(rs.getString("NOMBRE"));
+        empresa.setNOMBRE(rs.getString("CORREO"));
+        empresa.setTELEFONO(rs.getLong("TELEFONO"));
+        empresa.setID_USUARIO(rs.getLong("ID_USUARIO"));
+        if (rs.getDate("FECHA_RETIRO") != null) {
+            empresa.setFECHA_RETIRO(rs.getDate("FECHA_RETIRO").toLocalDate());
+        }
+        return empresa;
+    }
+
+}
--- coes2/src/main/java/com/example/coes2/dao/SolicitudDao.java
+++ coes2/src/main/java/com/example/coes2/dao/SolicitudDao.java
@@ -13,7 +13,57 @@ public class SolicitudDao implements Dao<Solicitud> {
     @Autowired
     JdbcTemplate template;
 
-    public List<Solicitud> obtenerTodos(){
-        List<Solicitud> res
+    public List<Solicitud> obtenerTodos() {
+        List<Solicitud> res = new ArrayList<>();
+        String sql = "SELECT * FROM SOLICITUD";
+        SqlRowSet rs = template.queryForRowSet(sql);
+        while (rs.next()) {
+            res.add(obtenerRegistro(rs));
+        }
+        return res;
     }
+
+    public void agregar(Solicitud solicitud) {
+        String sql = "INSERT INTO SOLICITUD(FECHA_EMISION, FECHA_TENTATIVA, COMENTARIO, ESTADO, ID_EMPRESA, ID_EQUIPO. ID_CENTRAL) VALUES(?,?,?,?,?,?,?)";
+        template.update(sql, solicitud.getFECHA_EMISION(), solicitud.getFECHA_TENTATIVA(), solicitud.getCOMENTARIO(),
+                solicitud.getESTADO(), solicitud.getID_EMPRESA(), solicitud.getID_EQUIPO(), solicitud.getID_CENTRAL());
+    }
+
+    public Solicitud buscar(Long id) {
+        Solicitud solicitud = null;
+        String sql = "SELECT * FROM SOLICITUD WHERE ID_SOLICITUD=?";
+        SqlRowSet rs = template.queryForRowSet(sql, id);
+        if (rs.next()) {
+            solicitud = obtenerRegistro(rs);
+        }
+        return solicitud;
+    }
+
+    public void actualizar(Solicitud solicitud) {
+        String sql = "UPDATE SOLICITUD SET FECHA_EMISION = ?, FECHA_TENTATIVA=?)" + "WHERE ID_SOLICITUD=?";
+        template.update(sql, solicitud.getFECHA_EMISION(), solicitud.getFECHA_TENTATIVA(), solicitud.getID_SOLICITUD());
+    }
+
+    public void eliminar(Solicitud solicitud) {
+        String sql = "DELETE FROM SOLICITUD WHERE ID_SOLICITUD=?";
+        template.update(sql, solicitud.getID_SOLICITUD());
+    }
+
+    private Solicitud obtenerRegistro(SqlRowSet rs) {
+        Solicitud solicitud = new Solicitud();
+        solicitud.setID_SOLICITUD(rs.getLong("ID_SOLICITUD"));
+        solicitud.setCOMENTARIO(rs.getString("COMENTARIO"));
+        solicitud.setESTADO(rs.getString("ESTADO"));
+        solicitud.setID_EMPRESA(rs.getLong("ID_EMPRESA"));
+        solicitud.setID_EQUIPO(rs.getLong("ID_EQUIPO"));
+        solicitud.setID_CENTRAL(rs.getLong("ID_CENTRAL"));
+        if (rs.getDate("FECHA_EMISION") != null) {
+            solicitud.setFECHA_EMISION(rs.getDate("FECHA_EMISION").toLocalDate());
+        }
+        if (rs.getDate("FECHA_TENTATIVA") != null) {
+            solicitud.setFECHA_EMISION(rs.getDate("FECHA_TENTATIVA").toLocalDate());
+        }
+        return solicitud;
+    }
+
 }
