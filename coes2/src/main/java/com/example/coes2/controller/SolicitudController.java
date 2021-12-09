1600c5cd0388b6bc2ce979ece41ae68bee069f6b
sebasjem1234 <sebasjem1234@gmail.com>
2021-12-08 22:20:32 -0500

    kk

--- coes2/src/main/java/com/example/coes2/controller/SolicitudController.java
+++ coes2/src/main/java/com/example/coes2/controller/SolicitudController.java
@@ -1,5 +1,23 @@
 package com.example.coes2.controller;
 
-public class SolicitudController {
+import java.util.List;
 
+import com.example.coes2.bean.Solicitud;
+import com.example.coes2.service.SolicitudService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/solicitudes")
+public class SolicitudController {
+    @Autowired
+    SolicitudService solicitudService;
+
+    @GetMapping
+    public List<Solicitud> obtenerTodos(){
+        return solicitudService.obtenerTodos();
+    }
 }
--- coes2/src/main/java/com/example/coes2/service/SolicitudService.java
+++ coes2/src/main/java/com/example/coes2/service/SolicitudService.java
@@ -1,5 +1,17 @@
 package com.example.coes2.service;
 
+import java.util.List;
+
+import com.example.coes2.bean.Solicitud;
+import com.example.coes2.dao.SolicitudDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class SolicitudService {
-    
+    @Autowired
+    SolicitudDao solicitudDao;
+
+    public List<Solicitud> obtenerTodos(){
+        return solicitudDao.obtenerTodos();
+    }
 }
