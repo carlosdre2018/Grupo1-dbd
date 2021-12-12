01baa8b5a1370b37d1a7dd230f55bb481e99b0a1
sebasjem1234 <sebasjem1234@gmail.com>
2021-12-11 21:36:12 -0500

    commit

--- coes2/src/main/java/com/example/coes2/bean/Ensayo.java
+++ coes2/src/main/java/com/example/coes2/bean/Ensayo.java
@@ -3,67 +3,67 @@ package com.example.coes2.bean;
 import java.time.LocalDate;
 
 public class Ensayo {
-    Long ID_ENSAYO;
-    String ESTADO_ENSAYO;
-    LocalDate FECHA_PROGRAMADA;
-    String COMENTARIO;
-    LocalDate FECHA_FIN;
-    Long ID_TRABAJADOR;
-    Long ID_SOLICITUD;
+    Long id_ensayo;
+    String estado_ensayo;
+    LocalDate fecha_programada;
+    String comentario;
+    LocalDate fecha_fin;
+    Long id_trabajador;
+    Long id_solicitud;
 
     public Long getID_ENSAYO() {
-        return ID_ENSAYO;
+        return id_ensayo;
     }
 
     public void setID_ENSAYO(Long iD_ENSAYO) {
-        ID_ENSAYO = iD_ENSAYO;
+        id_ensayo = iD_ENSAYO;
     }
 
     public String getESTADO_ENSAYO() {
-        return ESTADO_ENSAYO;
+        return estado_ensayo;
     }
 
     public void setESTADO_ENSAYO(String eSTADO_ENSAYO) {
-        ESTADO_ENSAYO = eSTADO_ENSAYO;
+        estado_ensayo = eSTADO_ENSAYO;
     }
 
     public LocalDate getFECHA_PROGRAMADA() {
-        return FECHA_PROGRAMADA;
+        return fecha_programada;
     }
 
     public void setFECHA_PROGRAMADA(LocalDate fECHA_PROGRAMADA) {
-        FECHA_PROGRAMADA = fECHA_PROGRAMADA;
+        fecha_programada = fECHA_PROGRAMADA;
     }
 
     public String getCOMENTARIO() {
-        return COMENTARIO;
+        return comentario;
     }
 
     public void setCOMENTARIO(String cOMENTARIO) {
-        COMENTARIO = cOMENTARIO;
+        comentario = cOMENTARIO;
     }
 
     public LocalDate getFECHA_FIN() {
-        return FECHA_FIN;
+        return fecha_fin;
     }
 
     public void setFECHA_FIN(LocalDate fECHA_FIN) {
-        FECHA_FIN = fECHA_FIN;
+        fecha_fin = fECHA_FIN;
     }
 
     public Long getID_TRABAJADOR() {
-        return ID_TRABAJADOR;
+        return id_trabajador;
     }
 
     public void setID_TRABAJADOR(Long iD_TRABAJADOR) {
-        ID_TRABAJADOR = iD_TRABAJADOR;
+        id_trabajador = iD_TRABAJADOR;
     }
 
     public Long getID_SOLICITUD() {
-        return ID_SOLICITUD;
+        return id_solicitud;
     }
 
     public void setID_SOLICITUD(Long iD_SOLICITUD) {
-        ID_SOLICITUD = iD_SOLICITUD;
+        id_solicitud = iD_SOLICITUD;
     }
 }
